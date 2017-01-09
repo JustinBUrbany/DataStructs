2c2155ae0f523abedf315993caa8cceb65a1a580
JustinBUrbany <Justinburbany@gmail.com>
2017-01-09 12:52:38 -0800

    Add files via upload

---
+++ array.h
@@ -0,0 +1,30 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+template<class T> 
+class Array
+{
+public:
+
+	Array();
+	Array(int length, int start_index = 0);
+	Array(const Array & copy);
+	~Array();
+	Array & operator =(const Array & rhs);
+	T & operator [](int index);
+	int getStartIndex();
+	void setStartIndex(int start_index);
+	int getLength();
+	void setLength(int length);
+
+private:
+	T * m_array;
+	int m_length;
+	int m_start_index;
+};
+
+
+
+
+
+#endif
